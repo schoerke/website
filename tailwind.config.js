556826758d315f609a97b24af8af15de49478a35
Scott Voyles <zeitchef@gmail.com>
2024-11-30 22:50:19 +0100

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
+  darkMode: ['selector', '[data-theme="dark"]'],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        '2xl': '2rem',
+        DEFAULT: '1rem',
+        lg: '2rem',
+        md: '2rem',
+        sm: '1rem',
+        xl: '2rem',
+      },
+      screens: {
+        '2xl': '86rem',
+        lg: '64rem',
+        md: '48rem',
+        sm: '40rem',
+        xl: '80rem',
+      },
+    },
+  },
+}
+
+export default config
