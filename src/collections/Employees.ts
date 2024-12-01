b201694a8bd50c7556ee5cb6d49d9942b46a6ee0
Scott Voyles <zeitchef@gmail.com>
2024-12-01 22:09:51 +0100

    refactor: rename Employee collection

--- src/collections/Employees.ts
+++ src/collections/Employees.ts
@@ -11,6 +11,11 @@ export const Employees: CollectionConfig = {
       type: 'text',
     },
     {
+      label: {
+        de: 'Titel',
+        en: 'Title',
+      },
+      localized: true,
       name: 'title',
       required: true,
       type: 'text',
@@ -21,11 +26,19 @@ export const Employees: CollectionConfig = {
       type: 'email',
     },
     {
+      label: {
+        de: 'Telefon',
+        en: 'Phone',
+      },
       name: 'phone',
       required: true,
       type: 'text',
     },
     {
+      label: {
+        de: 'Mobil',
+        en: 'Mobile',
+      },
       name: 'mobile',
       required: true,
       type: 'text',
@@ -39,5 +52,11 @@ export const Employees: CollectionConfig = {
       type: 'upload',
     },
   ],
+  labels: {
+    plural: {
+      de: 'Mitarbeiter',
+      en: 'Employees',
+    },
+  },
   slug: 'employees',
 }
