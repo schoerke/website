426117c992a55893cd7dfd2dfbd384d240214233
Scott Voyles <zeitchef@gmail.com>
2024-12-03 22:24:41 +0100

    chore: add data

---
+++ src/data/options.ts
@@ -0,0 +1,22 @@
+export const artistOptions = [
+  {
+    label: 'Artist - Maurice Steger',
+    value: 'maurice-steger',
+  },
+]
+
+export const categoryOptions = [
+  {
+    label: 'News',
+    value: 'news',
+  },
+  {
+    label: 'Projects',
+    value: 'projects',
+  },
+  {
+    label: 'Home',
+    value: 'home',
+  },
+  ...artistOptions,
+]
