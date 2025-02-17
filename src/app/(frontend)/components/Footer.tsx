07e5740c578ddf0de2e2b6368c2554119d9749dd
Scott Voyles <zeitchef@gmail.com>
2025-02-17 19:44:29 +0100

    refactor: update footer

--- src/app/(frontend)/components/Footer.tsx
+++ src/app/(frontend)/components/Footer.tsx
@@ -4,8 +4,8 @@ const Footer: React.FC = () => {
   return (
     <footer>
       <div className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
-          <div className="order-1 flex flex-col items-start">
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 xl:grid-cols-4">
+          <div className="order-1 flex flex-col items-start lg:col-start-1 xl:col-auto">
             <Image
               src="https://ik.imagekit.io/qy9qrvele0b/logo_icon.png?updatedAt=1739717132045"
               alt="Schoerke Logo"
@@ -15,7 +15,7 @@ const Footer: React.FC = () => {
             />
           </div>
 
-          <div className="order-3 mt-4 md:order-2 md:mt-0">
+          <div className="order-3 mt-4 md:mt-0 lg:col-start-2 lg:row-span-2 xl:col-auto">
             <ul className="space-y-3">
               {[
                 { text: 'Home', href: '/' },
@@ -23,7 +23,6 @@ const Footer: React.FC = () => {
                 { text: 'News', href: '/news' },
                 { text: 'Projects', href: '/projects' },
                 { text: 'Team', href: '/team' },
-                { text: 'Contact', href: '/contact' },
               ].map((link) => (
                 <li key={link.text}>
                   <a href={link.href} className="text-gray-600 transition duration-150 ease-in-out hover:text-gray-800">
@@ -34,10 +33,29 @@ const Footer: React.FC = () => {
             </ul>
           </div>
 
-          <div className="order-2 mt-4 md:order-3 md:mt-0">
+          <div className="order-4 mt-4 md:mt-0 lg:col-start-2 xl:col-auto">
+            <ul className="space-y-3">
+              {[
+                { text: 'Contact', href: '/contact' },
+                { text: 'Impressum', href: '/impressum' },
+                { text: 'Datenschutz', href: '/datenschutz' },
+              ].map((link) => (
+                <li key={link.text}>
+                  <a href={link.href} className="text-gray-600 transition duration-150 ease-in-out hover:text-gray-800">
+                    {link.text}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="order-2 mt-4 md:mt-0 lg:col-start-1 xl:order-4 xl:col-auto">
             <address className="not-italic">
               <div className="text-gray-600">
-                <p className="mb-2 text-xl font-bold">Künstlersekretariat Astrid Schoerke</p>
+                <p className="mb-2 text-xl font-bold">
+                  Künstlersekretariat <br className="hidden xl:block" />
+                  Astrid Schoerke
+                </p>
                 <p className="mb-2">Emanuel-Geibel-Str. 10</p>
                 <p className="mb-2">D-65185 Wiesbaden</p>
                 <p className="mb-2">
